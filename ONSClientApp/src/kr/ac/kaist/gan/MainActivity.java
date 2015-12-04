a9f69dbb160ca2f6f4713ba32580c84f10d5da29
ChungkwonRyu <ryuch91@kaist.ac.kr>
2015-12-04 22:37:03 +0900

    add libraries and fix some layout

--- ONSClientApp/src/kr/ac/kaist/gan/MainActivity.java
+++ ONSClientApp/src/kr/ac/kaist/gan/MainActivity.java
@@ -1,16 +1,61 @@
 package kr.ac.kaist.gan;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.util.Log;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import kr.ac.kaist.gan.SimpleSocketThread.MessageTypeClass;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final EditText ed1 = (EditText) findViewById(R.id.editText1);
+		Button buttonSend = (Button) findViewById(R.id.button1);
+		
+		Handler mHandler = new Handler(Looper.getMainLooper()){
+			@Override
+			public void handleMessage(Message inputMessage){
+				switch(inputMessage.what){
+				case MessageTypeClass.SIMSOCK_DATA :
+					String msg = (String) inputMessage.obj;
+					Log.d("OUT",  msg);
+					// do something with UI
+					break;
+				case MessageTypeClass.SIMSOCK_CONNECTED:
+					// do something with UI
+					break;
+				case MessageTypeClass.SIMSOCK_DISCONNECTED:
+					// do something with UI
+					break;
+				}
+			}
+		};
+		
+		final SimpleSocketThread ssocket = new SimpleSocketThread("192.168.0.1", 7777, mHandler);
+		ssocket.start();
+		
+		buttonSend.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				// TODO Auto-generated method stub
+				ssocket.sendString(ed1.getText());
+				ed1.setText("");
+			}
+		});
+		
+		
+
 	}
 
 	@Override
---
+++ ONSClientApp/src/kr/ac/kaist/gan/SimpleSocketThread.java
@@ -0,0 +1,110 @@
+package kr.ac.kaist.gan;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.util.Log;
+
+public class SimpleSocketThread extends Thread{
+	private Socket mSocket;
+	private BufferedReader buffRecv;
+	private BufferedWriter buffSend;
+	
+	private String mAddr = "localhost";
+	private int mPort = 7777;
+	private boolean mConnected = false;
+	private Handler mHandler = null;
+	
+	static class MessageTypeClass{
+		public static final int SIMSOCK_CONNECTED = 1;
+		public static final int SIMSOCK_DATA = 2;
+		public static final int SIMSOCK_DISCONNECTED = 3;
+	};
+	
+	public enum MessageType { SIMSOCK_CONNECTED, SIMSOCK_DATA, SIMSOCK_DISCONNECTED };
+	
+	public SimpleSocketThread(String addr, int port, Handler handler){
+		mAddr = addr;
+		mPort = port;
+		mHandler = handler;
+	}
+	
+	private void makeMessage(MessageType what, Object obj){
+		Message msg = Message.obtain();
+		msg.what = what.ordinal();
+		msg.obj = obj;
+		mHandler.sendMessage(msg);
+	}
+	
+	private boolean connect (String addr, int port){
+		try{
+			InetSocketAddress socketAddress = new InetSocketAddress(InetAddress.getByName(addr),port);
+			mSocket = new Socket();
+			mSocket.connect(socketAddress, 7777);
+		}catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void run(){
+		if(!connect(mAddr,mPort)) return; //connect failed
+		if(mSocket == null) return; //connect failed
+		
+		try {
+			buffRecv = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+			buffSend = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mConnected = true;
+
+		makeMessage(MessageType.SIMSOCK_CONNECTED, "");
+		Log.d("SimpleSocket", "socket_thread loop started");
+
+		String aLine = null;
+
+		while( !Thread.interrupted() ){ try {
+			aLine = buffRecv.readLine(); 
+			if(aLine != null) makeMessage(MessageType.SIMSOCK_DATA, aLine);
+			else break;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}
+
+		makeMessage(MessageType.SIMSOCK_DISCONNECTED, "");
+		Log.d("SimpleSocket", "socket_thread loop terminated");
+
+		try {
+			buffRecv.close(); 
+			buffSend.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mConnected = false;
+	}
+	
+	synchronized public boolean isConnected(){
+		return mConnected;
+	}
+	
+	public void sendString(Editable str){
+		PrintWriter out = new PrintWriter(buffSend, true);
+		out.println(str);
+	}
+}
